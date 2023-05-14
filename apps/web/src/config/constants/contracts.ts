b186fa945612b6621754e386ca42ebc6099d6288
towerdevswap <akmalhanif672@gmail.com>
2023-05-14 18:22:10 +0700

    first commit

---
+++ apps/web/src/config/constants/contracts.ts
@@ -0,0 +1,164 @@
+import { multicallAddresses } from '@pancakeswap/multicall'
+
+// Masterchef v1: 0xbeF35383ce0a377D618f309CCfCA7A702850817d
+
+export default {
+  masterChef: {
+    59140: '0x86AF084E7d9Ca3D7a435F69295cDFB34416A7eBd',
+    56: '0xd964CB5A5cE6B7c8fA26796760F8A5AeE9bee1a1',
+  },
+  masterChefV1: {
+    59140: '0x92ebcdA1e2C2d5B388d3f5450cF46930F0D179A0',
+    56: '0x60229Cdb2647803c42D9E37E71cDCAB489f6e859',
+  },
+  sousChef: {
+    59140: '',
+    56: '',
+  },
+  lotteryV2: {
+    59140: '',
+    56: '',
+  },
+  multiCall: multicallAddresses,
+  pancakeProfile: {
+    59140: '0x843887ac63f97dA742d35C7e150069ff5827Cf80',
+    56: '',
+  },
+  pancakeBunnies: {
+    59140: '0xadfa8f3558d4937603df64f286d405300d6ee32c',
+    56: '',
+  },
+  bunnyFactory: {
+    59140: '0x3997b0215D24984A7500645D2aD37ee2417B9e07',
+    56: '',
+  },
+  claimRefund: {
+    59140: '',
+    56: '',
+  },
+  pointCenterIfo: {
+    59140: '',
+    56: '',
+  },
+  bunnySpecial: {
+    59140: '',
+    56: '',
+  },
+  tradingCompetitionEaster: {
+    59140: '',
+    56: '',
+  },
+  tradingCompetitionFanToken: {
+    59140: '',
+    56: '',
+  },
+  tradingCompetitionMobox: {
+    59140: '',
+    56: '',
+  },
+  tradingCompetitionMoD: {
+    59140: '',
+    56: '',
+  },
+  easterNft: {
+    59140: '',
+    56: '',
+  },
+  cakeVault: {
+    56: '0x72f78a9B330cBA12F046BBf3C7D18610061fC65c',
+    59140: '',
+  },
+  cakeFlexibleSideVault: {
+    56: '0xD962DAE1d0DCF72dEfA8ad4dd37D34837ad61b2d',
+    59140: '',
+  },
+  predictionsBNB: {
+    59140: '',
+    56: '',
+  },
+  predictionsCAKE: {
+    59140: '',
+    56: '',
+  },
+  chainlinkOracleBNB: {
+    59140: '',
+    56: '',
+  },
+  chainlinkOracleCAKE: {
+    59140: '',
+    56: '',
+  },
+  predictionsV1: {
+    59140: '',
+    56: '',
+  },
+  bunnySpecialCakeVault: {
+    59140: '',
+    56: '',
+  },
+  bunnySpecialPrediction: {
+    59140: '',
+    56: '',
+  },
+  bunnySpecialLottery: {
+    59140: '',
+    56: '',
+  },
+  bunnySpecialXmas: {
+    59140: '',
+    56: '',
+  },
+  farmAuction: {
+    59140: '',
+    56: '',
+  },
+  AnniversaryAchievement: {
+    59140: '',
+    56: '',
+  },
+  nftMarket: {
+    59140: '0xf5d0451128064c1aa5b3dff43fa2608d97c2572d',
+    56: '',
+  },
+  nftSale: {
+    59140: '',
+    56: '',
+  },
+  pancakeSquad: {
+    59140: '0xEdFa035C3db4709ebcFDd88E722B012fcCDd2139',
+    56: '',
+  },
+  potteryDraw: {
+    59140: '0x4203646a8C1e452C54AC59ee34Bb1D101607Cac5',
+    56: '',
+  },
+  // TODO: multi
+  zap: {
+    59140: '',
+    56: '',
+  },
+  iCake: {
+    59140: '0x04f7462b2C5418aa93d17CB72921C82560415598',
+    56: '',
+  },
+  bCakeFarmBooster: {
+    59140: '',
+    56: '',
+  },
+  bCakeFarmBoosterProxyFactory: {
+    59140: '',
+    56: '',
+  },
+  nonBscVault: {
+    59140: '0x4EFe0F3A0a4d037b2770Db655C8c9708B15aeC88',
+    56: '',
+  },
+  crossFarmingSender: {
+    59140: '',
+    56: '',
+  },
+  crossFarmingReceiver: {
+    59140: '',
+    56: '',
+  },
+}
---
+++ apps/web/src/config/constants/supportChains.ts
@@ -0,0 +1,6 @@
+import { ChainId } from '@pancakeswap/sdk'
+
+export const SUPPORT_ONLY_BSC = [ChainId.LINEA_TESTNET]
+export const SUPPORT_FARMS = [ChainId.LINEA_TESTNET]
+
+export const SUPPORT_ZAP = [ChainId.BSC, ChainId.BSC_TESTNET]
